42de8bc9fc4d54aa2ca276d946b5d2c338130e4d
Tony Broyez <tony.broyez@consensys.net>
2024-05-07 22:58:01 +0200

    feat(variable): initial attempt at handling StateVariableDeclaration

--- main.go
+++ main.go
@@ -1,59 +1,248 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"gopkg.in/yaml.v3"
-	"io/ioutil"
-	"log"
 )
 
-type Contract struct {
-	State  []StateVariable             `yaml:"state"`
-	Defs   []Definition                `yaml:"defs"` // Changed from Functions to Defs
-	Events map[string][]EventParameter `yaml:"events"`
+// Define a generic AST node structure
+type ASTNode map[string]interface{}
+
+// Define structures for the input AST
+type Declarations struct {
+	ID struct {
+		Name string `json:"name"`
+		Type string `json:"type"`
+	} `json:"id"`
+	Initializer struct {
+		Type  string `json:"type"`
+		Value string `json:"value"`
+	} `json:"initializer"`
+	Type string `json:"type"`
 }
 
-type StateVariable struct {
-	Name    string `yaml:"name"`
-	Type    string `yaml:"type"`
-	Payable bool   `yaml:"payable,omitempty"`
+// Define structures for the output AST
+type ContractDefinition struct {
+	Type          string        `json:"type"`
+	Name          string        `json:"name"`
+	BaseContracts []interface{} `json:"baseContracts"`
+	SubNodes      []interface{} `json:"subNodes"`
+	Kind          string        `json:"kind"`
 }
 
-type Definition struct { // Renamed from Function to Definition
-	Name       string              `yaml:"name"`
-	Visibility []string            `yaml:"visibility,omitempty"` // Moved inside Definition
-	Type       string              `yaml:"type,omitempty"`       // Made optional
-	Parameters []FunctionParameter `yaml:"parameters,omitempty"` // Made optional
-	Locals     []LocalVariable     `yaml:"locals,omitempty"`     // Made optional
+type StateVariableDeclaration struct {
+	Type         string      `json:"type"`
+	Variables    []Variable  `json:"variables"`
+	InitialValue interface{} `json:"initialValue"`
 }
 
-type FunctionParameter struct {
-	Name string `yaml:"name"`
-	Type string `yaml:"type"`
+type Variable struct {
+	Type            string             `json:"type"`
+	TypeName        VariableTypeName   `json:"typeName"`
+	Name            string             `json:"name"`
+	Identifier      VariableIdentifier `json:"identifier"`
+	Expression      interface{}        `json:"expression"`
+	Visibility      string             `json:"visibility"`
+	IsStateVar      bool               `json:"isStateVar"`
+	IsDeclaredConst bool               `json:"isDeclaredConst"`
+	IsIndexed       bool               `json:"isIndexed"`
+	IsImmutable     bool               `json:"isImmutable"`
+	Override        interface{}        `json:"override"`
+	StorageLocation interface{}        `json:"storageLocation"`
 }
 
-type LocalVariable struct {
-	Name     string `yaml:"name"`
-	Type     string `yaml:"type"`
-	InMemory bool   `yaml:"inMemory,omitempty"`
+type VariableTypeName struct {
+	Type            string      `json:"type"`
+	Name            string      `json:"name"`
+	StateMutability interface{} `json:"stateMutability"`
 }
 
-type EventParameter struct {
-	Name string `yaml:"name"`
-	Type string `yaml:"type"`
+type VariableIdentifier struct {
+	Type string `json:"type"`
+	Name string `json:"name"`
+}
+
+type VariableExpression struct {
+	Type      string   `json:"type"`
+	Value     string   `json:"value"`
+	Parts     []string `json:"parts"`
+	IsUnicode []bool   `json:"isUnicode"`
+}
+
+type VariableInitialValue struct {
+	Type      string   `json:"type"`
+	Value     string   `json:"value"`
+	Parts     []string `json:"parts"`
+	IsUnicode []bool   `json:"isUnicode"`
 }
 
 func main() {
-	data, err := ioutil.ReadFile("playground.yaml")
-	if err != nil {
-		log.Fatalf("error: %v", err)
+
+	inputJSON := `{
+  "body": [
+    {
+      "declarations": [
+        {
+          "id": {
+            "name": "count",
+            "type": "Identifier"
+          },
+          "initializer": null,
+          "type": "VariableDeclaration"
+        }
+      ],
+      "type": "VariableStatement"
+    },
+    {
+      "body": {
+        "body": [
+          {
+            "argument": {
+              "left": {
+                "type": "NumericLiteral",
+                "value": 2
+              },
+              "operator": "*",
+              "right": {
+                "name": "count",
+                "type": "Identifier"
+              },
+              "type": "BinaryExpression"
+            },
+            "type": "ReturnStatement"
+          }
+        ],
+        "type": "BlockStatement"
+      },
+      "name": {
+        "name": "get",
+        "type": "Identifier"
+      },
+      "type": "FunctionDeclaration"
+    }
+  ],
+  "name": "Counter",
+  "type": "Contract"
+}`
+
+	var inputAST ASTNode
+	if err := json.Unmarshal([]byte(inputJSON), &inputAST); err != nil {
+		fmt.Println("Error parsing input JSON:", err)
+		return
 	}
 
-	var contract Contract
-	err = yaml.Unmarshal(data, &contract)
+	outputAST := transpileAST(inputAST)
+	outputJSON, err := json.MarshalIndent(outputAST, "", "  ")
 	if err != nil {
-		log.Fatalf("error: %v", err)
+		fmt.Println("Error generating output JSON:", err)
+		return
 	}
 
-	fmt.Printf("Parsed Contract: %+v\n", contract.Events)
+	fmt.Println(string(outputJSON))
+}
+
+// transpileAST transpiles the input AST to the desired output format.
+func transpileAST(inputAST ASTNode) ContractDefinition {
+	contractName, _ := inputAST["name"].(string)
+	outputAST := ContractDefinition{
+		Type:          "ContractDefinition",
+		Name:          contractName,
+		BaseContracts: []interface{}{},
+		SubNodes:      []interface{}{},
+		Kind:          "contract",
+	}
+
+	// Process each body element based on its type
+	for _, bodyElement := range inputAST["body"].([]interface{}) {
+		bodyNode := bodyElement.(map[string]interface{})
+		switch bodyNode["type"] {
+		case "VariableStatement":
+			processVariableStatement(&outputAST, bodyNode)
+		case "FunctionDeclaration":
+			processFunctionDeclaration(&outputAST, bodyNode)
+		}
+	}
+
+	return outputAST
+}
+
+// processVariableStatement processes a variable statement and adds it to the contract definition.
+func processVariableStatement(contractDef *ContractDefinition, node map[string]interface{}) {
+	// This function should extract information from the node and add a corresponding
+	// state variable declaration to the contractDef.SubNodes
+
+	varDeclarations, _ := node["declarations"].([]interface{})
+
+	for _, decl := range varDeclarations {
+
+		var varType, _ = decl.(map[string]interface{})["type"].(string)
+		var varIdName, _ = decl.(map[string]interface{})["id"].(map[string]interface{})["name"].(string)
+		var varInitializer, _ = decl.(map[string]interface{})["initializer"]
+		var varInitializerValue interface{}
+		var varInitializerType string
+		var varInitialValue interface{} = nil
+		var varExpression interface{} = nil
+
+		if varInitializer != nil {
+			varInitializerValue, _ = decl.(map[string]interface{})["initializer"].(map[string]interface{})["value"].(string)
+			varInitializerType, _ = decl.(map[string]interface{})["initializer"].(map[string]interface{})["type"].(string)
+
+			varInitialValue = VariableInitialValue{
+				Type:      varInitializerType,
+				Value:     varInitializerValue.(string),
+				Parts:     []string{varInitializerValue.(string)},
+				IsUnicode: []bool{false},
+			}
+
+			varExpression = VariableExpression{
+				Type:      "StringLiteral",
+				Value:     varInitializerValue.(string),
+				Parts:     []string{varInitializerValue.(string)},
+				IsUnicode: []bool{false},
+			}
+		}
+
+		println("@processVariableStatement, varDeclarations type", varType, varIdName, varInitializerValue, varInitializerType)
+
+		svd := StateVariableDeclaration{
+			Type: "StateVariableDeclaration",
+			Variables: []Variable{{
+				Type: "VariableDeclaration",
+				TypeName: VariableTypeName{
+					Type:            "ElementaryTypeName",
+					Name:            "string",
+					StateMutability: nil,
+				},
+				Name: varIdName,
+				Identifier: VariableIdentifier{
+					Type: "Identifier",
+					Name: varIdName,
+				},
+
+				Visibility: "<fill-with-yaml-spec>",
+				IsStateVar: true,
+				// IsDeclaredConst: false,
+				// IsIndexed:       false,
+				// IsImmutable:     false,
+			}},
+		}
+
+		if varExpression != nil {
+			svd.Variables[0].Expression = varExpression.(VariableExpression)
+		}
+
+		if varInitialValue != nil {
+			svd.InitialValue = varInitialValue.(VariableInitialValue)
+		}
+
+		contractDef.SubNodes = append(contractDef.SubNodes, svd)
+	}
+}
+
+// processFunctionDeclaration processes a function declaration and adds it to the contract definition.
+func processFunctionDeclaration(contractDef *ContractDefinition, node map[string]interface{}) {
+	// Implementation for processing function declarations
+	// This function should extract information from the node and add a corresponding
+	// function definition to the contractDef.SubNodes
+	println("@processFunctionDeclaration", node)
 }
